34dfce72a2b7db5cb988ea445fb021f4b21ac614
Javier <javier.fda.gervas@gmail.com>
2019-02-17 20:17:27 +0100

    errores varios
    ahora compila

--- graph.c
+++ graph.c
@@ -30,7 +30,8 @@ int find_node_index(const Graph * g, int nId1) {
 
 Bool* graph_getConectionsIndex(const Graph * g, int index) {    
 	Bool *array = NULL;
-	int i, j=0, size;        
+	int i, j=0, size;
+
 	if (!g) return NULL;    
 
 	if (index < 0 || index >g->num_nodes) return NULL;    
@@ -141,28 +142,27 @@ Status graph_setNode (Graph *g, const Node *n){
 
 		return ERROR;
 	}
-	node_setName(g->node[index])=node_getName(n);
-	node_setConnect(g->node[index])=node_getConnect(n);
+	node_setName(g->node[index], node_getName(n));
+	node_setConnect(g->node[index], node_getConnect(n));
 
 	return OK;
 }
 
 int * graph_getNodesId (const Graph * g){
 
-	int i;
-	int array;
+	int *ids, i;
 
-	array=(Graph*)malloc(g->num_nodes,sizeof(Graph));
+	ids = (int*)malloc(g->num_nodes*sizeof(int));
 
-	if(array==NULL){
-		return -1;
-	}
+	if(ids==NULL)
+		return NULL;
+	
+	for(i = 0; i < g->num_nodes; i++)
+		ids[i] = node_getId(g->node[i]);
 
-		for(i=0;i<g->num_nodes;i++){
-			array[i]=node_getId(g->node[i]);
-		}
 
-	return array;
+	return ids;
+
 }
 
 int graph_getNumberOfNodes(const Graph * g){
@@ -188,64 +188,13 @@ Bool graph_areConnected(const Graph * g, const int nId1, const int nId2){
 
 	int index1,index2;
 	int i;
-	Bool array[MAX_NUM_NODES];
 
 	index1=find_node_index(g,nId1);
 	index2=find_node_index(g,nId2);
 
-	array=graph_getConectionsIndex(g,index1);
-
-	for(i=0;i<strlen(array);i++){
-
-		if(array[i]==index2) 	
-			return TRUE;
-	}
-
-	
-	return FALSE;
+	return g->matrix[index1][index2];
 }
-
-
-int find_node_index(const Graph * g, int nId1) {    
-	int i;    
-	if (!g) return -1;
-
-	for(i=0; i < g->num_nodes; i++) {        
-
-		if (node_getId(g->node[i]) ==  nId1) 
-			return i;    
-
-	} /* ID not find   */
-	return -1; 
-} 
-
-Bool* graph_getConectionsIndex(const Graph * g, int index) {    
-	Bool *array = NULL;
-	int i, j=0, size;        
-	if (!g) return NULL;    
-
-	if (index < 0 || index >g->num_nodes) return NULL;    
-
-	/* get memory for the array with the connected nodes index */
-
-	size = node_getConnect (g->node[index]);    
-	array = (Bool*) malloc(sizeof(Bool) * size);    
-
-	if (!array) {        
-	/* print errorr message  */
-	fprintf (stderr, "%s\n", strerror(errno));        
-	return NULL;    
-	}        
-
-    /* assign values to the array with the indexes of the connected nodes  */  
-	for(i = 0; i< g->num_nodes; i++) {        
-		if (g->matrix[index][i] == TRUE) {            
-			array[j] = i;            
-			j++;        
-		}    
-	}        
-	return array; 
-} 
+ 
 
 Status graph_readFromFile (FILE *fin, Graph *g) {    
 	Node *n;    
