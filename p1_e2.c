33c7d76e4269ae9eb4a5af9e95105cdd616ee74c
Javier00 <jaierlo00@hotmail.com>
2019-02-18 09:55:39 +0100

    p1_e2 acabado, pero da errores

--- p1_e2.c
+++ p1_e2.c
@@ -10,7 +10,8 @@ int main(){
   Node *n1;
   Node *n2;
   Graph *g;
-
+  FILE *file;
+  file=fopen("file.txt","w");
   n1=node_ini();
   n2=node_ini();
   node_setId(n1,111);
@@ -18,11 +19,44 @@ int main(){
   node_setId(n2,222);
   node_setName(n2,"second");
   g=graph_ini();
-  graph_insertNode(g, n1);
-  graph_insertNode(g, n2);
-  graph_insertEdge(g, node_getId(n1), node_getId(n2));
+  fprintf(file,"Inserting node 1 ... result ...: ");
+  if(graph_insertNode(g, n1)==ERROR){
+  	fprintf(file,"0");
+  }else{
+  	fprintf(file,"1");
+  }
+  fprintf(file,"Inserting node 2 ... result ...: ");
+  if(graph_insertNode(g, n2)==ERROR){
+  	fprintf(file,"0");
+  }else{
+  	fprintf(file,"1");
+  }
+  graph_insertEdge(g, node_getId(n2), node_getId(n1));
+  fprintf(file,"Connected node 1 and node 2: ");
+  if(graph_areConnected(g,node_getId(n1), node_getId(n2))==TRUE){
+  	fprintf(file,"Yes");
+  }else{
+  	fprintf(file,"No");
+  }
+  
+  fprintf(file,"Connected node 2 and node 1: ");
 
+  if(graph_areConnected(g,node_getId(n2), node_getId(n1))==TRUE){
+  	fprintf(file,"Yes");
+  }else{
+  	fprintf(file,"No");
+  }
+  fprintf(file,"Inserting node 2 ... result ...: ");
+  if(graph_insertNode(g, n2)==ERROR){
+  	fprintf(file,"0");
+  }else{
+  	fprintf(file,"1");
+  }
+  fprintf(file,"Graph");
 
+  graph_print(file,g);
+
+  fclose(file);
   return 0;
 }
 
